bdf158e5582e47018c55d5250be8aab3f7117d53
aifreeborn <takeno2020@163.com>
2024-08-21 22:50:25 +0800

    BSP:compile default config

--- aibuild.sh
+++ aibuild.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+if [ $# -ne 1 -a $# -ne 2 ];then
+    echo "Usage: $0 [uboot | flash /dev/sdb]"
+    exit
+fi
+
+if [[ "$1" == "uboot" ]];then
+    make distclean
+    make mx6ull_14x14_evk_emmc_defconfig
+    make
+elif [[ "$1" == "flash" ]] && [[ -n $2 ]];then
+    sudo dd if=u-boot-dtb.imx of=$2 bs=1k seek=1 conv=fsync
+else
+    echo do nothing......
+fi
